e287c9b0ae57ad18fb08d36c5a94578bd1a827bf
Edrisrm <44209633+Edrisrm@users.noreply.github.com>
2020-09-14 17:03:44 -0600

    Tarea 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [count, saveCount] = useState(0);
+
+  // De forma similar a componentDidMount y componentDidUpdate
+  useEffect(() => {
+    // Actualiza el título del documento usando la API del navegador
+    document.title = `Número contador: ${count} `;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <div class="jumbotron">
+            <p class="display-4 text-center">Hola Mundo, Proyecto creado por Edris Ríos y Elvin Gonzales
+            <br/>
+            {count}
+            </p>
+          </div>
+          <p>Número de clicks {count}</p>
+          <div className="justify-content-center">
+            <button
+              className="btn btn-success"
+              onClick={() => saveCount(count + 1)}
+            >
+              Aumenta
+            </button>
+            <button
+              className="btn btn-danger"
+              onClick={() => {
+                if (count > 0) saveCount(count - 1);
+              }}
+            >
+              Decrece
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
